2d5f406fe1aacfba1d8d9f16b0851307d859ca42
putrikartikayuandari <113189445+putrikartikayuandari@users.noreply.github.com>
2023-09-22 14:36:49 +0700

    Add files via upload

---
+++ todolist/Config/TodoListRepository.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Repository {
+    use Entity\Todolist;
+
+    interface TodoListRepository
+    {
+        function save(Todolis $todolist): void;
+
+        function remove(int $number): bool;
+
+        function findAll(): array;
+    }
+
+    class TodoListRepositoryImpl implements TodoListRepository
+    {
+
+        public array $todolist = array();
+
+        private \PDO $connection;
+
+        public function __construct(\PDO $connection)
+        {
+            $this->connection = $connection;
+        }
+
+        function save (TodoList $todolist): void
+        {
+            // $number = sizeof($this->todolist) +1;
+            // $this->todolist[$number] = $todolist;
+
+            $sql = "INSERT INTO todolist(todo) VALUES (?)";
+            $statement = $this->connection->prepare($sql);
+            $statement->execute([todolist->getTodo()]);
+
+        }
+
+        function remove(int $number): bool
+        {
+            if ($number > sizeof($this->todolist)) {
+                return false;
+            }
+
+            for ($i = $number; $i < sizeof($this->todolist); $i++) {
+                $this->todolist[$i] = $this->todolist[$i + 1];
+            }
+
+            unset($this->todolist[sizeof($this->todolist)]);
+
+            return true;
+        }
+
+        function findAll(): array
+        {
+            return $this->todolist;
+        }
+
+    }
+}
